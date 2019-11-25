886b074dd997b2117adb1d0fdce19d91dc6f2a5d
David Clausen <david@poetic.io>
2019-11-25 09:24:55 -0600

    refactor: add headers, reduce payload, run linter

--- README.md
+++ README.md
@@ -1,8 +1,32 @@
 Run Lighthouse locally To See details in file
 lighthouse https://www.google.com/ --output=json --output-path=./reportNew.json --save-assets
 
-Run Locally (mockdata needs to be fixed)
-sls invoke local -f "lighthouse_lambda" --path tests/mockdata.json
+Run Locally
+
+Local uses the offline plugin to run the API gateway. Binds to port 7001 defined in serverless.yml
+
+```sh
+yarn local
+```
+
+The flag `--single-process` causes some OSX machines to hang, passing in custom flags below when testing locally can avoid the issue.
+
+example JSON body 
+```json
+{
+	"webhook": "http://localhost:4000/test/result",
+	"url": "https://www.google.com/",
+	"testId": "1231231",
+	"onlyChromeFlags": [
+		"--headless",
+		"--disable-dev-shm-usage",
+		"--disable-gpu",
+		"--no-zygote",
+		"--no-sandbox",
+		"--hide-scrollbars"
+	]
+}
+```
 
 lighthouse Architecture:
 https://github.com/GoogleChrome/lighthouse/blob/master/docs/architecture.md
--- index.js
+++ index.js
@@ -3,121 +3,139 @@ const lighthouse = require('lighthouse');
 const fetch = require('node-fetch');
 const _ = require('lodash');
 const chromeLauncher = require('chrome-launcher');
+const auditKeys = require('./auditKeys.json');
 
 let chromePath = undefined;
 
 // This lets us support invoke local
 if (!process.env.IS_LOCAL) {
-    chromePath = '/opt/bin/chromium';
+  chromePath = '/opt/bin/chromium';
 
-    // https://github.com/alixaxel/chrome-aws-lambda/blob/3779715fdc197a245af662725977133b2d676bf9/source/index.js#L6
-    // required for node10 support - makes sure fonts and shared libraries are loaded correctly
-    if (process.env.FONTCONFIG_PATH === undefined) {
-        process.env.FONTCONFIG_PATH = '/opt/lib';
-    }
+  // https://github.com/alixaxel/chrome-aws-lambda/blob/3779715fdc197a245af662725977133b2d676bf9/source/index.js#L6
+  // required for node10 support - makes sure fonts and shared libraries are loaded correctly
+  if (process.env.FONTCONFIG_PATH === undefined) {
+    process.env.FONTCONFIG_PATH = '/opt/lib';
+  }
 
-    if (
-        process.env.LD_LIBRARY_PATH &&
-        process.env.LD_LIBRARY_PATH.startsWith('/opt/lib:') !== true
-    ) {
-        process.env.LD_LIBRARY_PATH = [
-            ...new Set(['/opt/lib', ...process.env.LD_LIBRARY_PATH.split(':')]),
-        ].join(':');
-    }
+  if (
+    process.env.LD_LIBRARY_PATH &&
+    process.env.LD_LIBRARY_PATH.startsWith('/opt/lib:') !== true
+  ) {
+    process.env.LD_LIBRARY_PATH = [
+      ...new Set(['/opt/lib', ...process.env.LD_LIBRARY_PATH.split(':')]),
+    ].join(':');
+  }
 }
 
-const {
-    CHROME_FLAGS,
-    ONLY_CATEGORIES
-} = process.env;
+const { CHROME_FLAGS, ONLY_CATEGORIES } = process.env;
 
 let chromeFlags = CHROME_FLAGS.split(',');
 
 let lighthouseFlags = {
-    output: "json",
-    disableDeviceEmulation: false,
-    disableStorageReset: false,
-    throttlingMethod: "simulate",
+  output: 'json',
+  disableDeviceEmulation: false,
+  disableStorageReset: false,
+  throttlingMethod: 'simulate',
 };
 
 let lighthouseConfig = {
-    extends: 'lighthouse:default',
-    settings: {
-        onlyCategories: ONLY_CATEGORIES.split(','),
-    }
+  extends: 'lighthouse:default',
+  settings: {
+    onlyCategories: ONLY_CATEGORIES.split(','),
+  },
 };
 
 exports.handler = async (event) => {
-    console.log("BEGIN LAMBDA");
-    console.log(event.body);
+  console.log('BEGIN LAMBDA');
+  console.log(event.body);
 
-    let params = {};
-    params.cmd = JSON.parse(event.body);
+  let params = {};
+  params.cmd = JSON.parse(event.body);
 
-    const {
-        url,
-        webhook,
-        testId,
-        onlyChromeFlags,
-        onlyCategories,
-    } = _.get(params, "cmd");
+  const { url, webhook, testId, onlyChromeFlags, onlyCategories } = _.get(
+    params,
+    'cmd',
+  );
 
-    await checkJsonRequestForCustomProcessingOnLighthouse(onlyChromeFlags, onlyCategories);
+  console.log(url, webhook, testId, onlyChromeFlags, onlyCategories);
 
-    const results = await launchChromeAndRunLighthouse(url);
+  await checkJsonRequestForCustomProcessingOnLighthouse(
+    onlyChromeFlags,
+    onlyCategories,
+  );
 
-    const payload = {
-        url,
-        webhook,
-        testId,
-        testType: "lighthouse",
-        results
+  const { artifacts, report, ...results } = await launchChromeAndRunLighthouse(
+    url,
+  );
+
+  const payload = {
+    url,
+    webhook,
+    testId,
+    testType: 'lighthouse',
+    results: {
+      lhr: {
+        // Shapes the payload, audits too large too send during local testing
+        audits: auditKeys.reduce((acc, val) => {
+          acc[val] = results.lhr.audits[val];
+          return acc;
+        }, {}),
+      },
+    },
+  };
+
+  if (webhook) {
+    console.log(`Sending payload to ${webhook}`);
+    fetch(webhook, {
+      headers: { 'Content-Type': 'application/json' },
+      method: 'POST',
+      body: JSON.stringify(payload),
+    });
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify({ message: `sent payload to ${webhook}` }),
     };
+  } else {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify(payload),
+    };
+  }
+};
 
-    if (webhook) {
-        console.log(`Sending payload to ${webhook}`);
-        fetch(webhook, { method: 'POST', body: JSON.stringify(payload) });
-        return {
-            statusCode: 200,
-            headers: {
-                'Access-Control-Allow-Origin': '*'
-            },
-            body: JSON.stringify({ message: `sent payload to ${webhook}` })
-        };
-    } else {
-        return {
-            statusCode: 200,
-            headers: {
-                'Access-Control-Allow-Origin': '*'
-            },
-            body: JSON.stringify(payload)
-        };
-    }
-}
+async function checkJsonRequestForCustomProcessingOnLighthouse(
+  onlyChromeFlags,
+  onlyCategories,
+  onlyAudits,
+) {
+  console.log('Check Json Request For Custom Processing On Lighthouse');
 
-async function checkJsonRequestForCustomProcessingOnLighthouse(onlyChromeFlags, onlyCategories, onlyAudits) {
-    console.log("Check Json Request For Custom Processing On Lighthouse");
+  if (onlyChromeFlags) {
+    chromeFlags = onlyChromeFlags;
+  }
 
-    if (onlyChromeFlags) {
-        chromeFlags = onlyChromeFlags;
-    }
-
-    if (onlyCategories) {
-        lighthouseConfig.settings.onlyCategories = onlyCategories;
-    }
+  if (onlyCategories) {
+    lighthouseConfig.settings.onlyCategories = onlyCategories;
+  }
 }
 
 async function launchChromeAndRunLighthouse(url) {
-    console.log("Launch Chrome And Run Lighthouse");
+  console.log('Launch Chrome And Run Lighthouse');
 
-    try {
-        const chrome = await chromeLauncher.launch({ chromeFlags, chromePath });
-        lighthouseFlags.port = chrome.port;
-        const results = await lighthouse(url, lighthouseFlags, lighthouseConfig);
-        await chrome.kill();
-        return results;
-    } catch (error) {
-        console.error(error);
-        throw error;
-    }
+  try {
+    const chrome = await chromeLauncher.launch({ chromeFlags, chromePath });
+    lighthouseFlags.port = chrome.port;
+    const results = await lighthouse(url, lighthouseFlags, lighthouseConfig);
+    await chrome.kill();
+    return results;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 }
